363bfc4de2e5e30170aa260bbf03d2f40cf9f20f
Piotr Komisarczyk <88724625+pkomis@users.noreply.github.com>
2023-05-25 00:20:31 +0200

    Add files via upload

---
+++ player/player/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(384, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 51);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Open files";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(384, 216);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 51);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Shuffle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(384, 36);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(130, 95);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(84, 72);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(233, 161);
+            this.axWindowsMediaPlayer2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(554, 321);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+    }
+}
+
---
+++ player/player/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace player
+{
+    public partial class Form1 : Form
+    {
+        string[] doc, path;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = true;
+            open.Filter = "Mp3 Files|*.mp3|Avi Files|*avi|Wav Files|*.wav";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                doc = open.SafeFileNames;
+                path = open.SafeFileNames;
+                for (int i = 0; i < doc.Length; i++)
+                {
+                    listBox2.Items.Add(doc[i]);
+                }
+            }
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                axWindowsMediaPlayer1.URL = path[listBox2.SelectedIndex];
+            }
+            catch (IndexOutOfRangeException r)
+            {
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ListBox.ObjectCollection list = listBox2.Items;
+            Random random = new Random();
+            int w = list.Count;
+            listBox2.BeginUpdate();
+            while (w > 1)
+            {
+                w--;
+                int u = random.Next(w + 1);
+                object value = list[u];
+                list[u] = list[w];
+                list[w] = value;
+            }
+            listBox2.EndUpdate();   
+            listBox2.Invalidate();
+        }
+
+        
+
+        
+    }
+}
